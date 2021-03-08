2031c16ca84c7ac14a0ec4475cdd529a986a83e3
Sunil <36020672+sudo-sunil@users.noreply.github.com>
2021-03-09 01:35:30 +0530

    with js and jq

---
+++ Javascript/calcEvents.js
@@ -0,0 +1,18 @@
+function Add()
+{
+    var x=parseInt(document.getElementById("n1").value);
+    var y=parseInt(document.getElementById("n2").value);
+    alert("Add:"+(x+y));
+}
+function Sub()
+{
+
+}
+function Mul()
+{
+    
+}
+function Div()
+{
+
+}
